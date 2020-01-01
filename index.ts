eea3de61a8c74adc2833efc7ec4638551090694b
Will Nelson <will@pleb.xyz>
2019-12-31 20:36:37 -0800

    Update inputs

--- index.ts
+++ index.ts
@@ -6,10 +6,10 @@ which('bash', true).then(bash => {
 	return exec(bash, ['docs.sh'], {
 		env: {
 			REPO: `https://${process.env.GITHUB_ACTOR}:${getInput('githubToken')}@github.com/${process.env.GITHUB_REPOSITORY}.git`,
-			DOCS_DIRECTORY: getInput('docsDirectory'),
-			TARGET_BRANCH: getInput('targetBranch'),
-			TARGET_DIRECTORY: getInput('targetDirectory'),
-			DOCS_COMMAND: getInput('docsCommand'),
+			DOCS_DIRECTORY: getInput('docs-directory'),
+			TARGET_BRANCH: getInput('target-branch'),
+			TARGET_DIRECTORY: getInput('target-directory'),
+			DOCS_COMMAND: getInput('docs-command'),
 		},
 	});
 }).catch(error => {
